da44c51c4bc7ae78dc13b6b66d9edda4b862d2db
Marvelbad <badra.aqua@gmail.com>
2025-12-04 21:12:28 +0300

    Work in progress

--- src/main/java/com/badri/logging/service/UserService.java
+++ src/main/java/com/badri/logging/service/UserService.java
@@ -3,12 +3,23 @@ package com.badri.logging.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class UserService {
+    private final Map<String, String> dataBase = new HashMap<>();
+
     private static final Logger log = LoggerFactory.getLogger(UserService.class);
 
     public void createUser(String userName) {
-        log.info("Create user: {}", userName);
+        log.info("Attempt to create user {}", userName);
+        if (dataBase.containsKey(userName)) {
+            log.warn("User {} already exists", userName);
+            return;
+        }
+        dataBase.put(userName, userName);
+        log.info("User {} created", userName);
     }
 
     public void deleteUser(String userName) {
