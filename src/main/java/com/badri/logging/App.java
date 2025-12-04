ffe7bdc7adf9aa91c5e5da86fc2bb2eed301da4e
Marvelbad <badra.aqua@gmail.com>
2025-12-04 19:31:30 +0300

    Work in progress

--- src/main/java/com/badri/logging/App.java
+++ src/main/java/com/badri/logging/App.java
@@ -1,15 +1,21 @@
 package com.badri.logging;
 
 import com.badri.logging.service.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import java.util.UUID;
 
 public class App {
     public static void main(String[] args) {
+
+        MDC.put("requestId", UUID.randomUUID().toString());
+
         UserService service = new UserService();
 
         service.createUser("Badri");
         service.deleteUser("Alex");
         service.failUser("ErrorUser");
+
+        MDC.clear();
     }
 }
